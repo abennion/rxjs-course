00f6813198ee5a241d4353c8b8a09646e783d6ba
Alex Bennion <abennion@ezlinks.com>
2019-04-14 16:40:47 -0500

    saving

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Course } from "../model/course";
+import { interval, Observable, of, timer } from 'rxjs';
+import { catchError, delayWhen, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
 
 
 @Component({
@@ -10,16 +10,55 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    beginnerCourses$: Observable<Course[]>;
 
+    advancedCourses$: Observable<Course[]>;
 
     constructor() {
 
     }
 
     ngOnInit() {
+        const http$ = createHttpObservable('/api/courses');
 
+        const courses$ = http$.pipe(
+            map(res => Object.values(res['payload']))
+        )
 
+        this.beginnerCourses$ = http$
+            .pipe(
+                map(courses => courses.pipe(
+                    filter(course => course.category === 'BEGINNER'))
+                )
+            );
 
+        this.advancedCourses$ = http$
+            .pipe(
+                map(courses => courses.pipe(
+                    filter(course => course.category === 'ADVANCED'))
+                )
+            );
+
+        // const sub = courses$.subscribe(
+        //     (courses: any) => console.log(courses),
+        //     () => { },
+        //     () => console.log('completed')
+        // );
     }
+}
 
+function createHttpObservable(url: string) {
+    return Observable.create(observer => {
+        fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(body => {
+                observer.next(body);
+                observer.complete();
+            })
+            .catch(err => {
+                observer.error(err);
+            })
+    });
 }
