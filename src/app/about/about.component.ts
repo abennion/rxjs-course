fee9663fbc3eb312dfef4f28d96ad90cf2fea691
Alex Bennion <abennion@ezlinks.com>
2019-04-14 20:44:36 -0500

    fin 2.17

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { of, Observable, concat, interval } from 'rxjs';
+import { of, Observable, concat, interval, merge } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -12,14 +12,11 @@ export class AboutComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        const source1$ = of(1, 2, 3);
-        const source2$ = of(4, 5, 6);
-        const source3$ = of(7, 8, 9);
-        const result$ = concat(source1$, source2$, source3$);
+        const interval1$ = interval(1000);
+        const interval2$ = interval1$.pipe(map(val => 10 * val));
+        const result$ = merge(interval1$, interval2$);
 
-        result$.subscribe(
-            val => console.log(val)
-        );
+        result$.subscribe(console.log)
     }
 
 }
